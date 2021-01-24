52bf2889d38ac6adc7bad5df6a1e497308f07965
ALMaclaine <almaclaine@gmail.com>
2021-01-23 21:18:25 -0600

    general-utils: add makeId

--- packages/general-utils/src/general-utils.ts
+++ packages/general-utils/src/general-utils.ts
@@ -14,3 +14,13 @@ export class InvalidEmail extends ALMError {
 export function validateEmail(email) {
     return EmailValidator.validate(email);
 }
+
+export function makeId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const len = characters.length;
+    for (let i = 0; i < 16; i++) {
+        result += characters.charAt(Math.floor(Math.random() * len));
+    }
+    return result;
+}
